cdf74c4e3ebf62b23fd290318d9276b096009488
kairos1729 <kairos1729@users.noreply.github.com>
2019-10-20 16:09:27 +1100

    Initial commit

---
+++ src/main/java/guru/springframework/joke/controllers/JokesController.java
@@ -0,0 +1,21 @@
+package guru.springframework.joke.controllers;
+
+import guru.springframework.joke.services.JokesService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class JokesController {
+    private final JokesService jokesService;
+
+    public JokesController(JokesService jokesService) {
+        this.jokesService = jokesService;
+    }
+
+    @RequestMapping("/")
+    public String getAuthors(Model model){
+        model.addAttribute("joke", jokesService.getJoke());
+        return "chucknorris";
+    }
+}
---
+++ src/main/java/guru/springframework/joke/services/JokesService.java
@@ -0,0 +1,11 @@
+package guru.springframework.joke.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class JokesService {
+    public String getJoke() {
+        // Do something
+        return "blah";
+    }
+}
