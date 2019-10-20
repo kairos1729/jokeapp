0a1527a7135e588d31036cfb198d65e1f446bbe7
kairos1729 <kairos1729@users.noreply.github.com>
2019-10-20 16:28:43 +1100

    Split out interface for JokesService

--- src/main/java/guru/springframework/joke/services/JokesService.java
+++ src/main/java/guru/springframework/joke/services/JokesService.java
@@ -1,12 +1,5 @@
 package guru.springframework.joke.services;
 
-import guru.springframework.norris.chuck.ChuckNorrisQuotes;
-import org.springframework.stereotype.Service;
-
-@Service
-public class JokesService {
-    public String getJoke() {
-        var x = new ChuckNorrisQuotes();
-        return x.getRandomQuote();
-    }
+public interface JokesService {
+    String getJoke();
 }
