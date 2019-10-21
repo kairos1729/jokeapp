036cc83e7fb6eed660f51e4206991625c95da881
kairos1729 <kairos1729@users.noreply.github.com>
2019-10-22 10:15:45 +1100

    Add custom banner and java-based configuration

--- src/main/java/guru/springframework/joke/services/JokesServiceImpl.java
+++ src/main/java/guru/springframework/joke/services/JokesServiceImpl.java
@@ -6,8 +6,11 @@ import org.springframework.stereotype.Service;
 @Service
 public class JokesServiceImpl implements JokesService {
 
-    private final ChuckNorrisQuotes
-            chuckNorrisQuotes = new ChuckNorrisQuotes();
+    private final ChuckNorrisQuotes chuckNorrisQuotes;
+
+    public JokesServiceImpl(ChuckNorrisQuotes chuckNorrisQuotes) {
+        this.chuckNorrisQuotes = chuckNorrisQuotes;
+    }
 
     @Override
     public String getJoke() {
